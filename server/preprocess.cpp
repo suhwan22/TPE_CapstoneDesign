bd0ac6dee623f770a65293a6837b86966b235ce2
suhwan <tnghkswkd123@naver.com>
2023-10-17 20:41:30 +0900

    연장선 2

--- server/preprocess.cpp
+++ server/preprocess.cpp
@@ -49,18 +49,18 @@ unsigned char *preprocess(BMP_File *bmp_file, unsigned char *image)
                     b = image[row * bmp_file->bits * w + (col + 1) * bmp_file->bits + channel];
                     if (a + b > 255 && a + b <= 510 - a)
                     {
-                        pixel[row * bmp_file->bits * w + col * bmp_file->bits + channel] = 255 - temp_key[cnt % 256];
-                        pixel[row * bmp_file->bits * w + (col + 1) * bmp_file->bits + channel] = a + b + temp_key[cnt % 256] - 255;
+                        pixel[row * bmp_file->bits * w + col * bmp_file->bits + channel] = 255 - temp_key[rand() % 256];
+                        pixel[row * bmp_file->bits * w + (col + 1) * bmp_file->bits + channel] = a + b + temp_key[rand() % 256] - 255;
                     }
-                    else if (a + b < temp_key[cnt % 256])
+                    else if (a + b < temp_key[rand() % 256])
                     {
-                        pixel[row * bmp_file->bits * w + col * bmp_file->bits + channel] = temp_key[cnt % 256] - a;
-                        pixel[row * bmp_file->bits * w + (col + 1) * bmp_file->bits + channel] = temp_key[cnt % 256] - b;
+                        pixel[row * bmp_file->bits * w + col * bmp_file->bits + channel] = temp_key[rand() % 256] - a;
+                        pixel[row * bmp_file->bits * w + (col + 1) * bmp_file->bits + channel] = temp_key[rand() % 256] - b;
                     }
-					else if (a + b >= temp_key[cnt % 256])
+					else if (a + b >= temp_key[rand() % 256])
 					{
-                        pixel[row * bmp_file->bits * w + col * bmp_file->bits + channel] = a + b - temp_key[cnt % 256];
-                        pixel[row * bmp_file->bits * w + (col + 1) * bmp_file->bits + channel] = temp_key[cnt % 256];
+                        pixel[row * bmp_file->bits * w + col * bmp_file->bits + channel] = a + b - temp_key[rand() % 256];
+                        pixel[row * bmp_file->bits * w + (col + 1) * bmp_file->bits + channel] = temp_key[rand() % 256];
 					}
                 }
 			}
